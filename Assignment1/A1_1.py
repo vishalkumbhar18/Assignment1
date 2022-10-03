3be9cb2e8aedffa9e91b4df36c73057639cb3eb9
Vishal Kumbhar <86111799+vishalkumbhar18@users.noreply.github.com>
2022-10-03 21:23:22 +0530

    Add files via upload

---
+++ Assignment1/A1_1.py
@@ -0,0 +1,15 @@
+#1:>python --version
+import sys
+from wsgiref.simple_server import sys_version
+print(sys_version)
+print()
+#2:>print("hello python")
+#3:>
+
+
+
+print("Hello Python") #4
+print("Hello","Python",sep="\n")        #5
+
+print("\"MysirG\"")       #6
+print("\"Teacher's Day\"") #7
---
+++ Assignment1/A1_2.py
@@ -0,0 +1,13 @@
+a="VK"     #8
+print(a)
+name="Vishal" #9
+age=22
+qualification="B.E"
+yearsofexperience=0
+x=1
+y=2
+z=3
+print(name,age,qualification,yearsofexperience,sep="\n")
+print(x,y,z,sep="\n") #10b b  
+
+
